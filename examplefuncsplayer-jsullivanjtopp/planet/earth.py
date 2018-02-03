1291eb5ead328803c8eb231c4d93d0c6af9fafa7
jt_lc101 <joetopp@gmail.com>
2018-02-03 00:27:10 -0600

    initial mars plan

--- examplefuncsplayer-jsullivanjtopp/planet/earth.py
+++ examplefuncsplayer-jsullivanjtopp/planet/earth.py
@@ -6,7 +6,6 @@ import traceback
 directions = list(bc.Direction)
 max_rockets = 5
 
-
 def count_units(units, unitType):
     # counting units of a certain type
     result = 0
@@ -19,73 +18,72 @@ def count_units(units, unitType):
 def do_planet_tasks(gc: bc.GameController):
     my_team = gc.team()
     # walk through our units:
-    for unit in gc.my_units():
-        if unit.unit_type == bc.UnitType.Rocket:
-            print("I'm a rocket!")
-        # send some rockets
-        if unit.unit_type == bc.UnitType.Rocket:
-            if not unit.rocket_is_used():
-                if len(unit.structure_garrison()) > 7:
-                    mars_map = gc.starting_map(bc.Planet.Mars)
-                    # try 100 times to find a good landing location
-                    for i in range(100):
-                        x = random.randint(0, mars_map.width)
-                        y = random.randint(0, mars_map.height)
-                        landing = bc.MapLocation(bc.Planet.Mars, x, y)
-                        if gc.can_launch_rocket(unit.id, landing):
-                            print('launched a rocket with {} robots!'.format(len(unit.structure_garrison())))
-                            gc.launch_rocket(unit.id, landing)
+    if gc.round() < 500:
+        for unit in gc.my_units():
+            # send some rockets
+            if unit.unit_type == bc.UnitType.Rocket:
+                if not unit.rocket_is_used():
+                    if len(unit.structure_garrison()) > 7:
+                        mars_map = gc.starting_map(bc.Planet.Mars)
+                        # try 100 times to find a good landing location
+                        for i in range(100):
+                            x = random.randint(0, mars_map.width)
+                            y = random.randint(0, mars_map.height)
+                            landing = bc.MapLocation(bc.Planet.Mars, x, y)
+                            if gc.can_launch_rocket(unit.id, landing):
+                                print('launched a rocket with {} robots!'.format(len(unit.structure_garrison())))
+                                gc.launch_rocket(unit.id, landing)
+                                break
+                            else:
+                                print('cant land rocket at {}'.format(landing.to_json()))
+                else:
+                    gc.disintegrate_unit(unit.id)
+
+            # first, factory logic
+            if unit.unit_type == bc.UnitType.Factory:
+                garrison = unit.structure_garrison()
+                if len(garrison) > 0:
+                    d = random.choice(directions)
+                    if gc.can_unload(unit.id, d):
+                        print('unloaded a robot!')
+                        gc.unload(unit.id, d)
+                        continue
+                elif gc.can_produce_robot(unit.id, bc.UnitType.Worker):
+                    gc.produce_robot(unit.id, bc.UnitType.Worker)
+                    print('produced a worker!')
+                    continue
+
+            # first, let's look for nearby blueprints to work on
+            location = unit.location
+            if location.is_on_map():
+                nearby = gc.sense_nearby_units(location.map_location(), 3)
+                for other in nearby:
+                    if unit.unit_type == bc.UnitType.Worker and gc.can_build(unit.id, other.id):
+                        gc.build(unit.id, other.id)
+                        print('built a {}!'.format(other.unit_type.to_json()))
+                        # move onto the next unit
+                        continue
+                    if other.team != my_team and gc.is_attack_ready(unit.id) and gc.can_attack(unit.id, other.id):
+                        print('attacked a thing!')
+                        gc.attack(unit.id, other.id)
+                        continue
+                    if other.unit_type == bc.UnitType.Rocket and gc.can_load(other.id, unit.id):
+                        if not other.rocket_is_used():
+                            gc.load(other.id, unit.id)
                             break
-                        else:
-                            print('cant land rocket at {}'.format(landing.to_json()))
-            else:
-                gc.disintegrate_unit(unit.id)
 
-        # first, factory logic
-        if unit.unit_type == bc.UnitType.Factory:
-            garrison = unit.structure_garrison()
-            if len(garrison) > 0:
-                d = random.choice(directions)
-                if gc.can_unload(unit.id, d):
-                    print('unloaded a robot!')
-                    gc.unload(unit.id, d)
-                    continue
-            elif gc.can_produce_robot(unit.id, bc.UnitType.Worker):
-                gc.produce_robot(unit.id, bc.UnitType.Worker)
-                print('produced a worker!')
-                continue
+            # okay, there weren't any dudes around
+            # pick a random direction:
+            d = random.choice(directions)
 
-        # first, let's look for nearby blueprints to work on
-        location = unit.location
-        if location.is_on_map():
-            nearby = gc.sense_nearby_units(location.map_location(), 3)
-            for other in nearby:
-                if unit.unit_type == bc.UnitType.Worker and gc.can_build(unit.id, other.id):
-                    gc.build(unit.id, other.id)
-                    print('built a {}!'.format(other.unit_type.to_json()))
-                    # move onto the next unit
-                    continue
-                if other.team != my_team and gc.is_attack_ready(unit.id) and gc.can_attack(unit.id, other.id):
-                    print('attacked a thing!')
-                    gc.attack(unit.id, other.id)
-                    continue
-                if other.unit_type == bc.UnitType.Rocket and gc.can_load(other.id, unit.id):
-                    if not other.rocket_is_used():
-                        gc.load(other.id, unit.id)
-                        break
-
-        # okay, there weren't any dudes around
-        # pick a random direction:
-        d = random.choice(directions)
-
-        # or, try to build a rocket:
-        if gc.karbonite() > bc.UnitType.Rocket.blueprint_cost() and gc.can_blueprint(unit.id, bc.UnitType.Rocket, d) and count_units(gc.my_units(), bc.UnitType.Rocket) < max_rockets:
-            gc.blueprint(unit.id, bc.UnitType.Rocket, d)
-        elif gc.karbonite() > bc.UnitType.Factory.blueprint_cost() and gc.can_blueprint(unit.id, bc.UnitType.Factory, d):
-            gc.blueprint(unit.id, bc.UnitType.Factory, d)
-        elif gc.can_harvest(unit.id, d):
-            print("Harvesting Karbonite")
-            gc.harvest(unit.id, d)
-        # and if that fails, try to move
-        elif gc.is_move_ready(unit.id) and gc.can_move(unit.id, d):
-            gc.move_robot(unit.id, d)
+            # or, try to build a rocket:
+            if gc.karbonite() > bc.UnitType.Rocket.blueprint_cost() and gc.can_blueprint(unit.id, bc.UnitType.Rocket, d) and count_units(gc.my_units(), bc.UnitType.Rocket) < max_rockets:
+                gc.blueprint(unit.id, bc.UnitType.Rocket, d)
+            elif gc.karbonite() > bc.UnitType.Factory.blueprint_cost() and gc.can_blueprint(unit.id, bc.UnitType.Factory, d):
+                gc.blueprint(unit.id, bc.UnitType.Factory, d)
+            elif gc.can_harvest(unit.id, d):
+                print("Harvesting Karbonite")
+                gc.harvest(unit.id, d)
+            # and if that fails, try to move
+            elif gc.is_move_ready(unit.id) and gc.can_move(unit.id, d):
+                gc.move_robot(unit.id, d)
--- examplefuncsplayer-jsullivanjtopp/planet/mars.py
+++ examplefuncsplayer-jsullivanjtopp/planet/mars.py
@@ -5,7 +5,7 @@ import traceback
 
 directions = list(bc.Direction)
 max_workers = 5
-
+max_factories = 5
 
 def count_units(units, unitType):
     # counting units of a certain type
@@ -17,13 +17,89 @@ def count_units(units, unitType):
     return result
 
 def do_planet_tasks(gc: bc.GameController):
-    print("Working On Mars")
+    my_team = gc.team()
+
+    if my_team == bc.Team.Red:
+        enemy_team = bc.Team.Blue
+    else:
+        enemy_team = bc.Team.Red
+
     for unit in gc.my_units():
-        my_location = unit.location
+        location = unit.location
 
         # Unload the rockets
         if unit.unit_type == bc.UnitType.Rocket and len(unit.structure_garrison()) > 0:
             for d in directions:
                 if gc.can_unload(unit.id, d):
                     gc.unload(unit.id, d)
+                    print("Unloaded a " + unit.unit_type.to_json())
+            if len(unit.structure_garrison()) == 0:
+                gc.disintegrate_unit(unit.id)  # Destroy the empty rockets
 
+        # Factory logic
+        if unit.unit_type == bc.UnitType.Factory:
+            garrison = unit.structure_garrison()
+            # Unload produced units
+            if len(garrison) > 0:
+                d = random.choice(directions)
+                if gc.can_unload(unit.id, d):
+                    print('unloaded a robot!')
+                    gc.unload(unit.id, d)
+            # Focus on creating rangers
+            if count_units(gc.my_units(), bc.UnitType.Worker) >= max_workers:
+                if gc.can_produce_robot(unit.id, bc.UnitType.Ranger):
+                    gc.produce_robot(unit.id, bc.UnitType.Ranger)
+                    print('produced a ranger!')
+                    continue
+            # If we need to, make some workers
+            elif gc.can_produce_robot(unit.id, bc.UnitType.Worker):
+                gc.produce_robot(unit.id, bc.UnitType.Worker)
+                print('produced a worker!')
+
+        # Logic for Robots
+        if location.is_on_map():
+
+            # Rangers should attack everything in range
+            if unit.unit_type == bc.UnitType.Ranger:
+                nearby_enemies = gc.sense_nearby_units_by_team(location, unit.attack_range(), enemy_team)
+                if len(nearby_enemies) != 0:
+                    for enemy in nearby_enemies:
+                        if gc.can_attack(unit.id, enemy.id):
+                            gc.attack(unit.id, enemy.id)
+                            print('attacked a thing!')
+                            continue
+                elif gc.is_move_ready(unit.id):
+                    for d in directions:
+                        if gc.can_move(unit.id, d):
+                            gc.move_robot(unit.id, d)
+
+            # Workers have these priorities:
+            # 1. Build blueprints
+            # 2. Blueprint factories
+            # 3. Collect karbonite
+            # 4. Move
+            if unit.unit_type == bc.UnitType.Worker:
+
+                # Look for blueprints
+                nearby = gc.sense_nearby_units(location.map_location(), 1)
+                for other in nearby:
+                    if gc.can_build(unit.id, other.id):
+                        gc.build(unit.id, other.id)
+                        print('built a ' + other.unit_type.to_json())
+
+                # Lay blueprints
+                for d in directions:
+                    if gc.can_blueprint(unit.id, bc.UnitType.Factory, d):
+                        gc.blueprint(unit.id, bc.UnitType.Factory, d)
+                        print('laid a blueprint!')
+
+                # Mine karbonite
+                for d in directions:
+                    if gc.can_harvest(unit.id, d):
+                        gc.harvest(unit.id, d)
+                        print('collected karbonite!')
+
+                # Move
+                for d in directions:
+                    if gc.can_move(unit.id, d) and gc.is_move_ready(unit.id):
+                        gc.move_robot(unit.id, d)
